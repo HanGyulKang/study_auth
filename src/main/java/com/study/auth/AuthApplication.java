15c12b55761e2ebf61ceb0174a84c727f4108e5d
HanGyulKang <gang6607@gmail.com>
2022-05-29 17:43:17 +0900

    2022 05 29

--- src/main/java/com/study/auth/AuthApplication.java
+++ src/main/java/com/study/auth/AuthApplication.java
@@ -2,11 +2,28 @@ package com.study.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
+@RestController
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+	}
+
+	@GetMapping("/healthCheck")
+	public Mono<String> healthCheck() {
+		LocalDateTime now = LocalDateTime.now();
+		String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+
+		String data = "health : " + formatedNow;
+
+		return Mono.just(data);
 	}
 }
--- src/main/java/com/study/auth/controller/IndexController.java
+++ src/main/java/com/study/auth/controller/IndexController.java
@@ -1,13 +1,49 @@
 package com.study.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
+@RequestMapping("/auth")
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @GetMapping("/index")
+    public Mono<String> index() {
+        return Mono.just("index");
+    }
+
+    @GetMapping("/user")
+    public Mono<String> user() {
+        return Mono.just("user");
+    }
+
+    @GetMapping("/admin")
+    public Mono<String> admin() {
+        return Mono.just("admin");
+    }
+
+    @GetMapping("/manager")
+    public Mono<String> manager() {
+        return Mono.just("manager");
+    }
+
+    @GetMapping("/login")
+    public Mono<String> login() {
+        return Mono.just("login");
+    }
+
+    @GetMapping("/join")
+    public Mono<String> join() {
+        return Mono.just("join");
+    }
+
+    @GetMapping("/joinProc")
+    public Mono<String> joinProc() {
+        return Mono.just("회원가입 완료");
     }
 }
